cf2541ef9ef0bc04fb0c37c9ffd465ed1d9bb388
ozanmut <ozanmut@yahoo.com>
2018-12-11 22:25:10 +0300

    first commit

---
+++ TestAutomation/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TestAutomation
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnTest1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnTest1
+			// 
+			this.btnTest1.Location = new System.Drawing.Point(140, 79);
+			this.btnTest1.Name = "btnTest1";
+			this.btnTest1.Size = new System.Drawing.Size(75, 23);
+			this.btnTest1.TabIndex = 0;
+			this.btnTest1.Text = "Test Start";
+			this.btnTest1.UseVisualStyleBackColor = true;
+			this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 44);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Select web site";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(140, 37);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(412, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Image = global::TestAutomation.Properties.Resources.ie2;
+			this.btnBrowse.Location = new System.Drawing.Point(568, 33);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(30, 24);
+			this.btnBrowse.TabIndex = 5;
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(640, 192);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnTest1);
+			this.Name = "Form1";
+			this.Text = "Test Automation";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnTest1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btnBrowse;
+	}
+}
+
---
+++ TestAutomation/TestHelper.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomation
+{
+	public static class TestHelper
+	{
+		public static IWebElement FindElement(this IWebDriver driver, By by, TimeSpan timeout)
+		{
+			if (timeout.Seconds > 0)
+			{
+				var wait = new WebDriverWait(driver, timeout);
+				return wait.Until(Condition(by));
+			}
+			return driver.FindElement(by);
+		}
+
+		public static IWebDriver GetChromeDriver()
+		{
+			ChromeOptions options = new ChromeOptions();
+			IWebDriver driver = new ChromeDriver(options);
+			return driver;
+		}
+
+		public static Func<IWebDriver, IWebElement> Condition(By locator)
+		{
+			return (driver) => {
+				var element = driver.FindElements(locator).FirstOrDefault();
+				return element != null && element.Displayed && element.Enabled ? element : null;
+			};
+		}
+
+		public static void Login(IWebDriver driver)
+		{
+			var userNameElement = FindElement(driver, By.CssSelector("[ng-model='user.name']"), TimeSpan.FromSeconds(10));
+			userNameElement.SendKeys("admin");
+			var userPasswordElement = FindElement(driver, By.CssSelector("[ng-model='user.password']"), TimeSpan.FromSeconds(10));
+			userPasswordElement.SendKeys("123");
+
+			var loginButton = FindElement(driver, By.Id("btnLogin"), TimeSpan.FromSeconds(10));
+			loginButton.Click();
+		}
+		public static void Wait(IWebDriver driver, TimeSpan delay)
+		{
+			double interval = 250;
+			var now = DateTime.Now;
+			var wait = new WebDriverWait(driver, delay);
+			wait.PollingInterval = TimeSpan.FromMilliseconds(interval);
+			wait.Until(wd => (DateTime.Now - now) - delay > TimeSpan.Zero);
+		}
+
+		public static void WaitLoading(IWebDriver driver)
+		{
+			Wait(driver, TimeSpan.FromSeconds(5));
+			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(500));
+			wait.Until(d => !d.FindElement(By.CssSelector("[is-loading='loading']")).Displayed);
+		}
+
+		public static void AnalyzeClick(IWebDriver driver, string url)
+		{
+			driver.Navigate().GoToUrl(url);
+			Login(driver);
+			WaitLoading(driver);
+			var element = FindElement(driver, By.CssSelector("[ng-show='AnalysisVisible']"), TimeSpan.FromSeconds(10));
+
+			element.Click();
+		}
+
+	}
+}
